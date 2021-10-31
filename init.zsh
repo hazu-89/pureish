461f51e8644325119230e5dcb71ce180c86b9648
Hazu <83518756+hazu-89@users.noreply.github.com>
2021-10-30 22:32:09 -0500

    Added init script

---
+++ init.zsh
@@ -0,0 +1,14 @@
+fpath+=$HOME/.zsh/plugins/pure
+
+autoload -U promptinit; promptinit
+
+prompt pure
+
+source $HOME/.zsh/plugins/remote/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+
+source $HOME/.zsh/plugins/remote/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+for custom_script in $HOME/.zsh/custom/*.zsh(N); do
+	source custom_script
+done
+
